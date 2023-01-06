6a74bad58f6b2ade01eabf1a10a38041fcf2aa75
C4n4rd0 <drcd3@hotmail.com>
2023-01-06 17:58:00 +0100

    Delete jest-preset.js

--- jest-preset.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { resolve } = require('path');
-const tsJest = require('ts-jest/jest-preset');
-
-module.exports = {
-  ...tsJest,
-  globalSetup: resolve(__dirname, './jest-globalSetup-mix.js'),
-  globalTeardown: resolve(__dirname, './jest-globalTeardown-mix.js'),
-};
